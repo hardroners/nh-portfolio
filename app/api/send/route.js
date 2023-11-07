c29b137c83b0d93eadfecb980d3174d8393c1e0c
Hardroners <66667081+hardroners@users.noreply.github.com>
2023-11-07 19:52:08 +0100

    some changes

---
+++ app/api/send/route.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { NextResponse } from "next";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+export async function POST(req) {
+    console.log(email, subject, message);
+    try {
+        const { email, subject, message } = await req.json();
+        console.log(email, subject, message);
+        const data = await resend.emails.send({
+            from: fromEmail,
+            to: [fromEmail, email],
+            subject: subject,
+            react: (
+                <>
+                    <h1>{subject}</h1>
+                    <p>Thank you for contacting me!</p>
+                    <p>New message submitted:</p>
+                    <p>{message}</p>
+                </>
+            )
+        });
+
+        return NextResponse.json({ data });
+    } catch (error) {
+        return NextResponse.json({ error });
+    }
+}
